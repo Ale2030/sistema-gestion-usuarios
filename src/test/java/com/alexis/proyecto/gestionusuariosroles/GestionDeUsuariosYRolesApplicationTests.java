2a0ba95e5a48bc4f8cac2d306273c93818d8806b
Ale2030 <alexiscuevas551@gmail.com>
2024-11-16 14:49:01 -0300

    Primer commit

---
+++ src/test/java/com/alexis/proyecto/gestionusuariosroles/GestionDeUsuariosYRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexis.proyecto.gestionusuariosroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUsuariosYRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d1370ba4aa6e13fa6c12cad7bfd58ac1b83d5b22
Ale2030 <alexiscuevas551@gmail.com>
2024-11-16 19:48:20 -0300

    Eliminar los archivos application.properties y Connector.java del repositorio. Agregar ambos al .gitignore para proteger información sensible

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+## Archivo Connector.java
+
+El archivo `Connector.java` contiene valores ficticios para proteger la información sensible. 
+
+### ¿Qué hacer?
+1. Cambia la URL, el usuario y la contraseña según tu entorno local o servidor.
+2. Asegúrate de configurar correctamente la base de datos antes de ejecutar el proyecto.
+
 
---
+++ src/main/java/com/alexis/proyecto/gestionusuariosroles/connector/ConnectorExample.java
@@ -0,0 +1,25 @@
+package com.alexis.proyecto.gestionusuariosroles.connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectorExample {
+
+    private static String url = "jdbc:mariadb://example.com:3306/database_name";
+    private static String user = "example_user";
+    private static String pass = "example_password";
+    private static Connection conn = null;
+
+    private ConnectorExample() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la base de datos: " + e);
+        }
+        return conn;
+    }
+
+}

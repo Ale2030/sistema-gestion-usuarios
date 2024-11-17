9f9bfa15820a02039964f478acc7a13dfb6ffd77
Ale2030 <alexiscuevas551@gmail.com>
2024-11-16 21:01:52 -0300

    Se agrego el TestConnector

---
+++ src/test/java/com/alexis/proyecto/gestionusuariosroles/TestConnector.java
@@ -0,0 +1,35 @@
+package com.alexis.proyecto.gestionusuariosroles;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.alexis.proyecto.gestionusuariosroles.connector.Connector;
+
+@SpringBootTest
+public class TestConnector {
+
+    private Connection connection = Connector.getConnection();
+
+    @Test
+    public void TestDeConexion() {
+        try (ResultSet rs = connection
+                .createStatement()
+                .executeQuery("select version()")) {
+            if (rs.next()) {
+                String version = rs.getString(1);
+                if (version != null && !version.isEmpty()) {
+                    assertEquals(true, true);
+                } else {
+                    assertEquals(true, false);
+                }
+            } else {
+                assertEquals(true, false);
+            }
+        } catch (Exception e) {
+            assertEquals(true, false);
+        }
+    }
+
+}

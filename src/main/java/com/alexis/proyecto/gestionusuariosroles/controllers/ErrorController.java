7fadc1e546eabc73e11e47d3c1bde399f9b588ee
Ale2030 <alexiscuevas551@gmail.com>
2024-12-18 08:41:45 -0300

    Agregado de JavaDoc al controlador, mejora del HTML y boton de regresar

--- src/main/java/com/alexis/proyecto/gestionusuariosroles/controllers/ErrorController.java
+++ src/main/java/com/alexis/proyecto/gestionusuariosroles/controllers/ErrorController.java
@@ -3,9 +3,16 @@ package com.alexis.proyecto.gestionusuariosroles.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controlador para gestionar la vista de access-denied.
+ */
 @Controller
 public class ErrorController {
-
+    /**
+     * Muestra la vista access-denied.
+     * 
+     * @return la vista access-denied.
+     */
     @GetMapping("/access-denied")
     public String accessDenied() {
         return "access-denied";

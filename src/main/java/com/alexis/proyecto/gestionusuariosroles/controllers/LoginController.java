86ba59049bf3afd3fe0bb1857ac8a3d0cbed43f1
Ale2030 <alexiscuevas551@gmail.com>
2024-12-18 08:46:48 -0300

    Agregado de JavaDoc al controlador, mejora del HTML y responsividad

--- src/main/java/com/alexis/proyecto/gestionusuariosroles/controllers/LoginController.java
+++ src/main/java/com/alexis/proyecto/gestionusuariosroles/controllers/LoginController.java
@@ -2,10 +2,16 @@ package com.alexis.proyecto.gestionusuariosroles.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/**
+ * Controlador para gestionar la vista de login.
+ */
 @Controller
 public class LoginController {
-
+    /**
+     * Muestra la vista de login.
+     * 
+     * @return la vista de login.
+     */
     @GetMapping("/login")
     public String mostrarPagina() {
         return "login";

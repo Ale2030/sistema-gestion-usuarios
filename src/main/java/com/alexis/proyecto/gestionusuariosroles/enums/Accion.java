93b85cf0e4ac4a918c3736bb079860ab3f1d4190
Ale2030 <alexiscuevas551@gmail.com>
2024-12-18 09:08:33 -0300

    Agregado JavaDoc al enum de acciones.

--- src/main/java/com/alexis/proyecto/gestionusuariosroles/enums/Accion.java
+++ src/main/java/com/alexis/proyecto/gestionusuariosroles/enums/Accion.java
@@ -1,8 +1,10 @@
 package com.alexis.proyecto.gestionusuariosroles.enums;
-
+/**
+ * Enum representa las acciones que puede ejercer cada usuario.
+ * @author Alex
+ */
 public enum Accion {
     CREATE,
     UPDATE,
-    DELETE,
-    READ
+    DELETE
 }
